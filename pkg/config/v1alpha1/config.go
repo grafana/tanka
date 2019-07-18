b5773e662afcc48883602d96e0a6a17eb0fb48a9
sh0rez <me@shorez.de>
2019-07-18 12:27:02 +0200

    doc: Comments

--- pkg/config/v1alpha1/config.go
+++ pkg/config/v1alpha1/config.go
@@ -1,5 +1,7 @@
 package v1alpha1
 
+// Config holds the configuration variables for config version v1alpha1
+// ApiVersion and Kind are currently unused, this may change in the future.
 type Config struct {
 	APIVersion string       `json:"api_version"`
 	Kind       string       `json:"kind"`
@@ -7,8 +9,13 @@ type Config struct {
 	Spec       ProviderSpec `json:"spec"`
 }
 
+// Metadata is meant for humans and not parsed
 type Metadata struct {
 	Labels map[string]interface{} `json:"labels"`
 }
 
+// ProviderSpec is used to dynamically configure providers.
+// The providers are iterated over and the first valid provider will be used.
+// All properties specified inside of the provider are settings specific
+// to the provider itself.
 type ProviderSpec map[string]interface{}
