f35f757df2a6c4f3da0c274e28b635b5f9d687ff
sh0rez <me@shorez.de>
2019-08-06 13:20:49 +0200

    feat(tooling): import analysis
    Adds a tool (`tk tool imports`) to get a list of all files, a file transitively imports.
    
    Optionally allows to check whether a git commit changed one of those imports.
    
    BREAKING: `tk debug jpath` is now `tk tool jpath`

---
+++ cmd/tk/tool.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	"github.com/sh0rez/tanka/pkg/jpath"
+	"github.com/sh0rez/tanka/pkg/jsonnet"
+)
+
+func toolCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Short: "handy utilities for working with jsonnet",
+		Use:   "tool",
+	}
+	cmd.AddCommand(jpathCmd())
+	cmd.AddCommand(importsCmd())
+	return cmd
+}
+
+func jpathCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Short: "print information about the jpath",
+		Use:   "jpath",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			pwd, err := os.Getwd()
+			if err != nil {
+				return err
+			}
+			path, base, root := jpath.Resolve(pwd)
+			fmt.Println("main:", filepath.Join(base, "main.jsonnet"))
+			fmt.Println("rootDir:", root)
+			fmt.Println("baseDir:", base)
+			fmt.Println("jpath:", path)
+			return nil
+		},
+	}
+	return cmd
+}
+
+func importsCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "imports [file]",
+		Short: "list all transitive imports of a file",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			var modFiles []string
+			if cmd.Flag("check").Changed {
+				argv := []string{"diff-tree", "--no-commit-id", "--name-only", "-r", cmd.Flag("check").Value.String()}
+				c := exec.Command("git", argv...)
+				c.Stderr = os.Stderr
+				var buf bytes.Buffer
+				c.Stdout = &buf
+				if err := c.Run(); err != nil {
+					log.Fatalln("Invoking git:", err)
+				}
+				modFiles = strings.Split(buf.String(), "\n")
+			}
+
+			f, err := filepath.Abs(args[0])
+			if err != nil {
+				log.Fatalln("Opening file:", err)
+			}
+
+			deps, err := jsonnet.TransitiveImports(f)
+			if err != nil {
+				log.Fatalln("resolving imports:", err)
+			}
+
+			if modFiles != nil {
+				for _, m := range modFiles {
+					for _, d := range deps {
+						if m == d {
+							fmt.Printf("Rebuild required. File `%s` imports `%s`, which has been changed in `%s`.\n", args[0], d, cmd.Flag("check").Value.String())
+							os.Exit(16)
+						}
+					}
+				}
+				fmt.Printf("Rebuild not required, because no imported files have been changed in `%s`.\n", cmd.Flag("check").Value.String())
+				os.Exit(0)
+			}
+
+			s, err := json.Marshal(deps)
+			if err != nil {
+				log.Fatalln("Formatting:", err)
+			}
+			fmt.Println(string(s))
+		},
+	}
+
+	cmd.Flags().StringP("check", "c", "", "git commit hash to check against")
+
+	return cmd
+}
--- pkg/jsonnet/jsonnet.go
+++ pkg/jsonnet/jsonnet.go
@@ -38,3 +38,53 @@ func Evaluate(sonnet string, jpath []string) (string, error) {
 
 	return vm.EvaluateSnippet("main.jsonnet", sonnet)
 }
+
+type ImportVisitor func(who, what string) error
+
+func VisitImportsFile(jsonnetFile string, v ImportVisitor) error {
+	bytes, err := ioutil.ReadFile(jsonnetFile)
+	if err != nil {
+		return err
+	}
+
+	jpath, _, _ := jpath.Resolve(filepath.Dir(jsonnetFile))
+	return VisitImports(string(bytes), jpath, v)
+}
+
+func VisitImports(sonnet string, jpath []string, v ImportVisitor) error {
+	importer := TraceImporter{
+		JPaths:  jpath,
+		Visitor: v,
+	}
+
+	vm := jsonnet.MakeVM()
+	vm.Importer(&importer)
+	for _, nf := range native.Funcs() {
+		vm.NativeFunction(nf)
+	}
+
+	if _, err := vm.EvaluateSnippet("main.jsonnet", sonnet); err != nil {
+		return err
+	}
+	return nil
+}
+
+type TraceImporter struct {
+	JPaths   []string
+	Visitor  ImportVisitor
+	importer *jsonnet.FileImporter
+}
+
+func (t *TraceImporter) Import(importedFrom, importedPath string) (contents jsonnet.Contents, foundAt string, err error) {
+	if t.importer == nil {
+		t.importer = &jsonnet.FileImporter{
+			JPaths: t.JPaths,
+		}
+	}
+
+	contents, foundAt, err = t.importer.Import(importedFrom, importedPath)
+	if err := t.Visitor(importedFrom, foundAt); err != nil {
+		return jsonnet.Contents{}, "", err
+	}
+	return
+}
