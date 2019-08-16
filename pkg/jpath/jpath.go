cf73ac60116c9ab4417b01b85dc16817f56b1c3d
sh0rez <me@shorez.de>
2019-08-16 19:37:38 +0200

    fix(kubernetes): do not fail on nil receiver
    Although Tanka can run totally fine without a spec.json, it panicked because of
    the namespace override.
    
    Now leaves the namespace untouched if it is not known.
    
    Furthermore also fails on apply and diff without a spec.json with a clean error
    message.

--- pkg/jpath/jpath.go
+++ pkg/jpath/jpath.go
@@ -9,10 +9,10 @@ import (
 
 var (
 	// ErrorNoRoot means no rootDir was found in the parents
-	ErrorNoRoot = errors.New("could not locate a jsonnetfile.json in the parent directories, which is required to identify the project root")
+	ErrorNoRoot = errors.New("could not locate a jsonnetfile.json in the parent directories, which is required to identify the project root. Refer to https://tanka.dev/directory-structure for more information")
 
 	// ErrorNoBase means no baseDir was found in the parents
-	ErrorNoBase = errors.New("could not locate a main.jsonnet in the parent directories, which is required as the entrypoint for the evaluation")
+	ErrorNoBase = errors.New("could not locate a main.jsonnet in the parent directories, which is required as the entrypoint for the evaluation. Refer to https://tanka.dev/directory-structure for more information")
 )
 
 // ErrorFileNotFound means that the searched file was not found
