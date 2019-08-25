df086dee2bba0be0256960c1d9d378363cff7c6c
sh0rez <me@shorez.de>
2019-08-25 13:27:51 +0200

    fix(kubernetes): correct handling of kubectl stderr (#61)
    Always shows stderr from kubectl in case an error occurs

--- pkg/kubernetes/errors.go
+++ pkg/kubernetes/errors.go
@@ -3,11 +3,12 @@ package kubernetes
 import "fmt"
 
 type ErrorNotFound struct {
-	resource string
+	name string
+	kind string
 }
 
 func (e ErrorNotFound) Error() string {
-	return fmt.Sprintf(`error from server (NotFound): secrets "%s" not found`, e.resource)
+	return fmt.Sprintf(`error from server (NotFound): %s "%s" not found`, e.kind, e.name)
 }
 
 type ErrorMissingConfig struct {
