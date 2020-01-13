294fdc06c3355d0796d27fe133a5792d372d72b6
sh0rez <me@shorez.de>
2020-01-13 16:50:00 +0100

    test(kubernetes): reconcile ignore nil

--- pkg/kubernetes/reconcile_test.go
+++ pkg/kubernetes/reconcile_test.go
@@ -34,6 +34,15 @@ func TestExtract(t *testing.T) {
 			name: "array",
 			data: testDataArray(),
 		},
+		{
+			name: "nil",
+			data: func() testData {
+				d := testDataRegular()
+				d.deep.(map[string]interface{})["disabledObject"] = nil
+				return d
+			}(),
+			err: nil, // we expect no error, just the result of testDataRegular
+		},
 	}
 
 	for _, c := range tests {
