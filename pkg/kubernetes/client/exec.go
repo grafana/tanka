a5d39de0b6c1dd16f54744ad3ea0e0fce1952b81
sh0rez <me@shorez.de>
2020-02-12 18:48:33 +0100

    test(kubernetes): $KUBECONFIG patching

--- pkg/kubernetes/client/exec.go
+++ pkg/kubernetes/client/exec.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"sort"
 	"strings"
 )
 
@@ -18,24 +19,27 @@ func (k Kubectl) ctl(action string, args ...string) *exec.Cmd {
 
 	// prepare the cmd
 	cmd := exec.Command("kubectl", argv...)
-
-	// prepend namespace patch to $KUBECONFIG
-	env := newEnv()
-	if _, ok := env["KUBECONFIG"]; !ok {
-		env["KUBECONFIG"] = "~/.kube/config"
-	}
-	env["KUBECONFIG"] = fmt.Sprintf("%s:%s", k.nsPatch, env["KUBECONFIG"])
-	cmd.Env = env.render()
+	cmd.Env = patchKubeconfig(k.nsPatch, os.Environ())
 
 	return cmd
+}
+
+func patchKubeconfig(file string, e []string) []string {
+	// prepend namespace patch to $KUBECONFIG
+	env := newEnv(e)
+	if _, ok := env["KUBECONFIG"]; !ok {
+		env["KUBECONFIG"] = "~/.kube/config" // kubectl default
+	}
+	env["KUBECONFIG"] = fmt.Sprintf("%s:%s", file, env["KUBECONFIG"])
+	return env.render()
 }
 
 // environment is a helper type for manipulating os.Environ() more easily
 type environment map[string]string
 
-func newEnv() environment {
+func newEnv(e []string) environment {
 	env := make(environment)
-	for _, s := range os.Environ() {
+	for _, s := range e {
 		kv := strings.SplitN(s, "=", 2)
 		env[kv[0]] = kv[1]
 	}
@@ -47,5 +51,6 @@ func (e environment) render() []string {
 	for k, v := range e {
 		s = append(s, fmt.Sprintf("%s=%s", k, v))
 	}
+	sort.Strings(s)
 	return s
 }
---
+++ pkg/kubernetes/client/exec_test.go
@@ -0,0 +1,36 @@
+package client
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const patchFile = "/tmp/tk-nsPatch.yaml"
+
+func TestPatchKubeconfig(t *testing.T) {
+
+	cases := []struct {
+		name string
+		env  []string
+		want []string
+	}{
+		{
+			name: "none",
+			env:  []string{},
+			want: []string{"KUBECONFIG=" + patchFile + ":~/.kube/config"},
+		},
+		{
+			name: "custom",
+			env:  []string{"KUBECONFIG=/home/user/.config/kube"},
+			want: []string{"KUBECONFIG=" + patchFile + ":/home/user/.config/kube"},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := patchKubeconfig(patchFile, c.env)
+			assert.Equal(t, c.want, got)
+		})
+	}
+}
