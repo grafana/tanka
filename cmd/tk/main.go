010826b54050220eb9979a91c8aee2dda26dc2fe
sh0rez <me@shorez.de>
2020-02-29 21:09:18 +0100

    feat(kubernetes): cleanup environment patch
    Adds global defer using a custom package so that we can schedule tasks
    for run on exit.
    
    This is used for removing the environment patch from the temp directory
    after each run, so we don't leak possibly sensitive data there

--- cmd/tk/main.go
+++ cmd/tk/main.go
@@ -12,6 +12,7 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 
 	"github.com/grafana/tanka/pkg/cli/cmp"
+	"github.com/grafana/tanka/pkg/defers"
 	"github.com/grafana/tanka/pkg/jsonnet/jpath"
 	"github.com/grafana/tanka/pkg/spec"
 	"github.com/grafana/tanka/pkg/spec/v1alpha1"
@@ -80,6 +81,9 @@ func main() {
 		}
 		_ = cmd.Help()
 	}
+
+	// run global defers at exit
+	defer defers.Run()
 
 	// Run!
 	if err := rootCmd.Execute(); err != nil {
