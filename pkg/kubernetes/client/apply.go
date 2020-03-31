84eea1c6fca8854011270aaf241b3065a322fd73
Milan Plzik <milan.plzik@grafana.com>
2020-03-31 10:27:13 +0200

    Fix review comments.
    Signed-off-by: Milan Plzik <milan.plzik@grafana.com>

--- pkg/kubernetes/client/apply.go
+++ pkg/kubernetes/client/apply.go
@@ -7,51 +7,8 @@ import (
 	"github.com/grafana/tanka/pkg/kubernetes/manifest"
 )
 
-// Order in which install different kinds of Kubernetes objects.
-// Inspired by https://github.com/helm/helm/blob/8c84a0bc0376650bc3d7334eef0c46356c22fa36/pkg/releaseutil/kind_sorter.go
-var kindOrder = []string{
-	"Namespace",
-	"NetworkPolicy",
-	"ResourceQuota",
-	"LimitRange",
-	"PodSecurityPolicy",
-	"PodDisruptionBudget",
-	"ServiceAccount",
-	"Secret",
-	"ConfigMap",
-	"StorageClass",
-	"PersistentVolume",
-	"PersistentVolumeClaim",
-	"CustomResourceDefinition",
-	"ClusterRole",
-	"ClusterRoleList",
-	"ClusterRoleBinding",
-	"ClusterRoleBindingList",
-	"Role",
-	"RoleList",
-	"RoleBinding",
-	"RoleBindingList",
-	"Service",
-	"DaemonSet",
-	"Pod",
-	"ReplicationController",
-	"ReplicaSet",
-	"Deployment",
-	"HorizontalPodAutoscaler",
-	"StatefulSet",
-	"Job",
-	"CronJob",
-	"Ingress",
-	"APIService",
-}
-
 // Apply applies the given yaml to the cluster
 func (k Kubectl) Apply(data manifest.List, opts ApplyOpts) error {
-	// Manifests have already been pre-sorted during the Reconcile phase.
-	return k.apply(data, opts)
-}
-
-func (k Kubectl) apply(data manifest.List, opts ApplyOpts) error {
 	argv := []string{"-f", "-"}
 	if opts.Force {
 		argv = append(argv, "--force")
