62a2eb501cd265d38151a08b8d3eb082626463a3
Duologic <jeroen@simplistic.be>
2020-12-19 20:03:41 +0100

    refactor(cli): move findBaseDirs to tanka package

--- cmd/tk/args.go
+++ cmd/tk/args.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"os"
+
 	"github.com/go-clix/cli"
 	"github.com/posener/complete"
+
+	"github.com/grafana/tanka/pkg/tanka"
 )
 
 var workflowArgs = cli.Args{
 	Validator: cli.ValidateExact(1),
 	Predictor: cli.PredictFunc(func(args complete.Args) []string {
-		if dirs := findBaseDirs(); len(dirs) != 0 {
+		pwd, err := os.Getwd()
+		if err != nil {
+			return nil
+		}
+
+		dirs, err := tanka.FindBaseDirs(pwd)
+		if err != nil {
+			return nil
+		}
+
+		if len(dirs) != 0 {
 			return dirs
 		}
 
