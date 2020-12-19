84a274362a7beb2188f6afe07e899755fefb5236
Duologic <jeroen@simplistic.be>
2020-12-19 20:30:14 +0100

    refactor(cli): use tanka.FindEnvironments()

--- pkg/tanka/environments.go
+++ pkg/tanka/environments.go
@@ -4,7 +4,10 @@ import (
 	"os"
 	"path/filepath"
 
+	"k8s.io/apimachinery/pkg/labels"
+
 	"github.com/grafana/tanka/pkg/jsonnet/jpath"
+	"github.com/grafana/tanka/pkg/spec/v1alpha1"
 )
 
 const BASEDIR_INDICATOR = "main.jsonnet"
@@ -12,7 +15,7 @@ const BASEDIR_INDICATOR = "main.jsonnet"
 // FindBaseDirs searches for possible environments
 func FindBaseDirs(workdir string) (dirs []string, err error) {
 	_, _, _, err = jpath.Resolve(workdir)
-	if err == jpath.ErrorNoRoot {
+	if err != nil {
 		return nil, err
 	}
 
@@ -27,4 +30,31 @@ func FindBaseDirs(workdir string) (dirs []string, err error) {
 		return nil, err
 	}
 	return dirs, nil
+}
+
+// FindEnvironments searches for actual environments
+// ignores main.jsonnet if no environments found
+func FindEnvironments(workdir string, selector labels.Selector) (envs []v1alpha1.Environment, err error) {
+	dirs, err := FindBaseDirs(workdir)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, dir := range dirs {
+		_, env, err := ParseEnv(dir, ParseOpts{Evaluator: EnvsOnlyEvaluator})
+		if err != nil {
+			switch err.(type) {
+			case ErrNoEnv:
+				continue
+			default:
+				return nil, err
+			}
+		}
+
+		if selector == nil || selector.Empty() || selector.Matches(env.Metadata) {
+			envs = append(envs, *env)
+		}
+	}
+
+	return envs, nil
 }
