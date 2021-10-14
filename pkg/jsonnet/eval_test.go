a3777378acfca2cd6585def443d057a2057bd52f
Julien Duchesne <julien.duchesne@grafana.com>
2021-10-13 21:51:42 -0400

    Fix `std.thisFile` - This was broken here: https://github.com/grafana/tanka/pull/603 when I mistakenly assumed that EvaluateFile and EvaluateAnonymousSnippet were the same - I reverted the behavior so that we are now, again, using `vm.EvaluateFile` in the `EvaluateFile` function - I also added a test to prevent regressions

---
+++ pkg/jsonnet/eval_test.go
@@ -0,0 +1,18 @@
+package jsonnet
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// To be consistent with the jsonnet executable,
+// when evaluating a file, `std.thisFile` should point to the given path
+func TestEvaluateFile(t *testing.T) {
+	result, err := EvaluateFile("testdata/thisFile/main.jsonnet", Opts{})
+	assert.NoError(t, err)
+	assert.Equal(t, `{
+   "test": "testdata/thisFile/main.jsonnet"
+}
+`, result)
+}
---
+++ pkg/jsonnet/testdata/thisFile/main.jsonnet
@@ -0,0 +1,3 @@
+{
+  test: std.thisFile,
+}
