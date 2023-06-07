a474f0f58829124c71fa183258d6c50690986f39
Julien Duchesne <julien.duchesne@grafana.com>
2023-06-07 07:40:01 -0400

    Fix issues with docs website (#876)
    * Fix issues with docs website
    Closes https://github.com/grafana/tanka/issues/732
    Closes https://github.com/grafana/tanka/issues/797
    
    These issues come from the fact that we have custom code for the sidebar
    This custom code adds a logo (I moved that to the header) + algolia search (not very useful IMO, just search the github repo)
    
    * Hide helper .mdx files

--- docs/src/gatsby-theme-docz/components/Logo/index.js
+++ docs/src/gatsby-theme-docz/components/Logo/index.js
@@ -3,23 +3,27 @@ import * as styles from "gatsby-theme-docz/src/components/Logo/styles"
 /** @jsx jsx */
 import { Flex, jsx } from "theme-ui"
 import { Link, useConfig } from "docz"
+import logo from '../../../../img/logo.svg'
 
 export const Logo = () => {
-  const config = useConfig()
-  return (
-    <Flex alignItems="center" sx={styles.logo} data-testid="logo">
-      <Link
-        to="/"
-        sx={{
-          ...styles.link,
-          display: "flex",
-          flexDirection: "column",
-          lineHeight: "1.2",
-        }}
-      >
-        <span>Grafana Tanka</span>
-        <span sx={{ fontSize: "0.7em" }}>{config.description}</span>
-      </Link>
-    </Flex>
-  )
+    const config = useConfig()
+    return (
+        <Flex alignItems="center" sx={styles.logo} data-testid="logo">
+            <Link
+                to="/"
+                sx={{
+                    ...styles.link,
+                    display: "flex",
+                    flexDirection: "column",
+                    lineHeight: "1.2",
+                }}
+            >
+                <div sx={{ display: 'inline-block' }}>
+                    <img src={logo} alt="That's my logo" sx={{ maxHeight: "1.2em", marginRight: '0.2em', display: 'inline-block', float: 'left' }} />
+                    <span sx={{ display: 'inline-block' }}>Grafana Tanka</span>
+                    <span sx={{ fontSize: "0.7em", display: 'block' }}>{config.description}</span>
+                </div>
+            </Link>
+        </Flex>
+    )
 }
