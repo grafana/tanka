f9bfb579c0124b37d580ba1fc8b82e11bb9116fe
julienduchesne <julienduchesne@live.com>
2023-06-07 11:45:03 +0000

    Deploying to gh-pages from @ grafana/tanka@a474f0f58829124c71fa183258d6c50690986f39 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1abe1951fc17e94fbe0f.js"
+    "url": "webpack-runtime-c38f913d195ac363d90b.js"
   },
   {
     "url": "framework-3d5d6f3c5ba6d5f455c5.js"
@@ -39,14 +39,14 @@ self.__precacheManifest = [
     "url": "styles-95d00f891faf7b06d026.js"
   },
   {
-    "url": "f0e45107-de48ac461cc5ac1214b6.js"
+    "url": "f0e45107-91cefa4606c78384043e.js"
   },
   {
-    "url": "app-df67246a25ea4350d84a.js"
+    "url": "app-1193ea6251313e491564.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d593d977541d696d2b048b71c3a7785f"
+    "revision": "5beb28a6dd91828aee9ad5d1bbf5942d"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-099d9276a41f4ba01a3c.js"
@@ -55,7 +55,7 @@ self.__precacheManifest = [
     "url": "polyfill-8d34c62e07e660a83f29.js"
   },
   {
-    "url": "component---docs-introduction-mdx-2f488764d32620398d47.js"
+    "url": "component---docs-introduction-mdx-53424fcd5b068bb753bd.js"
   },
   {
     "url": "page-data/index/page-data.json",
@@ -63,21 +63,21 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/sq/d/1635659820.json",
-    "revision": "d3aad4c11479e6160ccd44cef04ffeb7"
+    "revision": "721f4f082687d5ea60fe94299d30ae8f"
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "85191cf9aa6000feedc84ababa6ab2d5"
+    "revision": "b9d6034f4af0900f86b33021c08fc3b6"
   },
   {
-    "url": "component---docs-installation-mdx-44b9a7a94980b54ae26c.js"
+    "url": "component---docs-installation-mdx-e9c5f51053a74f5a8f46.js"
   },
   {
     "url": "page-data/install/page-data.json",
-    "revision": "5de2f6c35bd5eab44b36f293d9e1acd0"
+    "revision": "70e59f6d7f4c06980a518910dbeba161"
   },
   {
-    "url": "component---docs-tutorial-overview-mdx-cd7095e341e9866fb7ca.js"
+    "url": "component---docs-tutorial-overview-mdx-d06a238fa15586c0de9b.js"
   },
   {
     "url": "page-data/tutorial/overview/page-data.json",
@@ -177,7 +177,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-df67246a25ea4350d84a.js`))) {
+  if (!resources || !(await caches.match(`/app-1193ea6251313e491564.js`))) {
     return await fetch(event.request)
   }
 
