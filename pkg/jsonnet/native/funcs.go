ded2b97f7e4ac2b36d94dd5da26d3a57094c735a
Rudolf Thomas <rudolf.thomas@firma.seznam.cz>
2023-09-15 11:39:08 +0200

    refactor: Make wrapNativeFunc less cumbersome to use.
    No-op functionality-wise.

--- pkg/jsonnet/native/funcs.go
+++ pkg/jsonnet/native/funcs.go
@@ -43,88 +43,93 @@ func Funcs() []*jsonnet.NativeFunction {
 	}
 }
 
-// wrapNativeFunc returns a wrapper around impl, which must be a function with
-// return types (interface{}, error).
-// The wrapper converts the untyped parameters from jsonnet to the types impl
-// expects, passes them to impl (note: the values are converted, not just
-// assigned). When unexpected types are passed on the jsonnet side, the wrapper
-// never calls impl and returns a human-readable error instead.
-// The wrapper is meant to be set as the given NativeFunction's Func field.
-func wrapNativeFunc(f *jsonnet.NativeFunction, impl interface{}) func([]interface{}) (interface{}, error) {
+// wrapNativeFunc takes a function name, names of the parameters (in a single
+// comma-separated string), and an implementation - a function with return
+// types (interface{}, error).
+// It produces a jsonnet.NativeFunction which has:
+//   - Name from given name
+//   - Params derived from given paramNamesStr
+//   - Func that converts the untyped parameters from the jsonnet side to the
+//     types impl expects, passes them to impl (note: the values are converted,
+//     not just assigned). When unexpected number and/or types of parameters
+//     are passed on the jsonnet side, the Func never calls impl and returns a
+//     human-readable error instead.
+func wrapNativeFunc(name, paramNamesStr string, impl interface{}) *jsonnet.NativeFunction {
 	implV := reflect.ValueOf(impl)
 	implT := implV.Type()
 	if implV.Kind() != reflect.Func || implV.IsNil() {
-		panic(fmt.Errorf("wrapNativeFunc(%s): not a non-nil function", f.Name))
+		panic(fmt.Errorf("wrapNativeFunc(%s): not a non-nil function", name))
 	}
-	if implT.NumIn() != len(f.Params) {
-		panic(fmt.Errorf("wrapNativeFunc(%s): wrong number of input parameters", f.Name))
+	var paramNames ast.Identifiers
+	for _, name := range strings.Split(paramNamesStr, ",") {
+		paramNames = append(paramNames, ast.Identifier(name))
+	}
+	if implT.NumIn() != len(paramNames) {
+		panic(fmt.Errorf("wrapNativeFunc(%s): wrong number of input parameters", name))
 	}
 	var goodOutTypesFunc func() (interface{}, error)
 	outTypesT := reflect.TypeOf(goodOutTypesFunc)
 	if implT.NumOut() != outTypesT.NumOut() ||
 		implT.Out(0) != outTypesT.Out(0) || implT.Out(1) != outTypesT.Out(1) {
-		panic(fmt.Errorf("wrapNativeFunc(%s): incorrect return parameters", f.Name))
+		panic(fmt.Errorf("wrapNativeFunc(%s): incorrect return parameters", name))
 	}
-	return func(params []interface{}) (interface{}, error) {
-		if len(params) != implT.NumIn() {
-			// Bug. Jsonnet should call us with the correct parameters.
-			panic(fmt.Errorf("%s(): wrong number of parameters", f.Name))
-		}
-		callParams := make([]reflect.Value, len(params))
-		for i, v := range params {
-			if v == nil {
-				return nil, fmt.Errorf("%s(): argument %#v is null", f.Name, f.Params[i])
+	return &jsonnet.NativeFunction{
+		Name:   name,
+		Params: paramNames,
+		Func: func(params []interface{}) (interface{}, error) {
+			if len(params) != implT.NumIn() {
+				// Bug. Jsonnet should call us with the correct parameters.
+				panic(fmt.Errorf("%s(): wrong number of parameters", name))
 			}
-			dstT := implT.In(i)
-			srcV := reflect.ValueOf(v)
-			if !srcV.CanConvert(dstT) {
-				return nil, fmt.Errorf("%s(): argument %#v has unexpected type", f.Name, f.Params[i])
+			callParams := make([]reflect.Value, len(params))
+			for i, v := range params {
+				if v == nil {
+					return nil, fmt.Errorf("%s(): argument %#v is null", name, paramNames[i])
+				}
+				dstT := implT.In(i)
+				srcV := reflect.ValueOf(v)
+				if !srcV.CanConvert(dstT) {
+					return nil, fmt.Errorf("%s(): argument %#v has unexpected type", name, paramNames[i])
+				}
+				callParams[i] = srcV.Convert(dstT)
 			}
-			callParams[i] = srcV.Convert(dstT)
-		}
-		results := implV.Call(callParams)
-		var outErr error
-		reflect.ValueOf(&outErr).Elem().Set(results[1])
-		return results[0].Interface(), outErr
+			results := implV.Call(callParams)
+			var outErr error
+			reflect.ValueOf(&outErr).Elem().Set(results[1])
+			return results[0].Interface(), outErr
+		},
 	}
 }
 
 // parseJSON wraps `json.Unmarshal` to convert a json string into a dict
 func parseJSON() *jsonnet.NativeFunction {
-	f := &jsonnet.NativeFunction{
-		Name:   "parseJson",
-		Params: ast.Identifiers{"json"},
-	}
-	f.Func = wrapNativeFunc(f,
+	return wrapNativeFunc(
+		"parseJson",
+		"json",
 		func(data []byte) (res interface{}, err error) {
 			err = json.Unmarshal(data, &res)
 			return
-		})
-	return f
+		},
+	)
 }
 
 func hashSha256() *jsonnet.NativeFunction {
-	f := &jsonnet.NativeFunction{
-		Name:   "sha256",
-		Params: ast.Identifiers{"str"},
-	}
-	f.Func = wrapNativeFunc(f,
+	return wrapNativeFunc(
+		"sha256",
+		"str",
 		func(data []byte) (interface{}, error) {
 			h := sha256.New()
 			h.Write(data)
 			return fmt.Sprintf("%x", h.Sum(nil)), nil
 		},
 	)
-	return f
 }
 
 // parseYAML wraps `yaml.Unmarshal` to convert a string of yaml document(s) into a (set of) dicts
 func parseYAML() *jsonnet.NativeFunction {
-	f := &jsonnet.NativeFunction{
-		Name:   "parseYaml",
-		Params: ast.Identifiers{"yaml"},
-	}
-	f.Func = wrapNativeFunc(f,
+	return wrapNativeFunc(
+		"parseYaml",
+		"yaml",
 		func(data []byte) (interface{}, error) {
 			ret := []interface{}{}
 
@@ -151,17 +156,15 @@ func parseYAML() *jsonnet.NativeFunction {
 			}
 
 			return ret, nil
-		})
-	return f
+		},
+	)
 }
 
 // manifestJSONFromJSON reserializes JSON which allows to change the indentation.
 func manifestJSONFromJSON() *jsonnet.NativeFunction {
-	f := &jsonnet.NativeFunction{
-		Name:   "manifestJsonFromJson",
-		Params: ast.Identifiers{"json", "indent"},
-	}
-	f.Func = wrapNativeFunc(f,
+	return wrapNativeFunc(
+		"manifestJsonFromJson",
+		"json,indent",
 		func(data []byte, indent int) (interface{}, error) {
 			data = bytes.TrimSpace(data)
 			buf := bytes.Buffer{}
@@ -170,17 +173,15 @@ func manifestJSONFromJSON() *jsonnet.NativeFunction {
 			}
 			buf.WriteString("\n")
 			return buf.String(), nil
-		})
-	return f
+		},
+	)
 }
 
 // manifestYamlFromJSON serializes a JSON string as a YAML document
 func manifestYAMLFromJSON() *jsonnet.NativeFunction {
-	f := &jsonnet.NativeFunction{
-		Name:   "manifestYamlFromJson",
-		Params: ast.Identifiers{"json"},
-	}
-	f.Func = wrapNativeFunc(f,
+	return wrapNativeFunc(
+		"manifestYamlFromJson",
+		"json",
 		func(data []byte) (interface{}, error) {
 			var input interface{}
 			if err := json.Unmarshal(data, &input); err != nil {
@@ -188,50 +189,44 @@ func manifestYAMLFromJSON() *jsonnet.NativeFunction {
 			}
 			output, err := yaml.Marshal(input)
 			return string(output), err
-		})
-	return f
+		},
+	)
 }
 
 // escapeStringRegex escapes all regular expression metacharacters
 // and returns a regular expression that matches the literal text.
 func escapeStringRegex() *jsonnet.NativeFunction {
-	f := &jsonnet.NativeFunction{
-		Name:   "escapeStringRegex",
-		Params: ast.Identifiers{"str"},
-	}
-	f.Func = wrapNativeFunc(f,
+	return wrapNativeFunc(
+		"escapeStringRegex",
+		"str",
 		func(s string) (interface{}, error) {
 			return regexp.QuoteMeta(s), nil
-		})
-	return f
+		},
+	)
 }
 
 // regexMatch returns whether the given string is matched by the given re2 regular expression.
 func regexMatch() *jsonnet.NativeFunction {
-	f := &jsonnet.NativeFunction{
-		Name:   "regexMatch",
-		Params: ast.Identifiers{"regex", "string"},
-	}
-	f.Func = wrapNativeFunc(f,
+	return wrapNativeFunc(
+		"regexMatch",
+		"regex,string",
 		func(regex, s string) (interface{}, error) {
 			return regexp.MatchString(regex, s)
-		})
-	return f
+		},
+	)
 }
 
 // regexSubst replaces all matches of the re2 regular expression with another string.
 func regexSubst() *jsonnet.NativeFunction {
-	f := &jsonnet.NativeFunction{
-		Name:   "regexSubst",
-		Params: ast.Identifiers{"regex", "src", "repl"},
-	}
-	f.Func = wrapNativeFunc(f,
+	return wrapNativeFunc(
+		"regexSubst",
+		"regex,src,repl",
 		func(regex, src, repl string) (interface{}, error) {
 			r, err := regexp.Compile(regex)
 			if err != nil {
 				return "", err
 			}
 			return r.ReplaceAllString(src, repl), nil
-		})
-	return f
+		},
+	)
 }
