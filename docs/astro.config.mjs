3380a36eb5c84751c8065acb9eed3100f2106c43
Giordano Ricci <me@giordanoricci.com>
2024-05-15 14:14:19 +0100

    fix base path

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tanka.dev',
+  base: process.env.PATH_PREFIX,
   integrations: [
     starlight({
       plugins: [starlightLinksValidator()],
