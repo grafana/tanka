abf646bb31f7bb72911e6eaaea29ad1a371aa035
Horst Gutmann <horst.gutmann@grafana.com>
2024-06-26 17:07:24 +0200

    Add testcase for tk export

--- acceptance-tests/help_test.go
+++ acceptance-tests/help_test.go
@@ -37,6 +37,28 @@ func TestApplyEnvironment(t *testing.T) {
 	runCmd(t, "kubectl", "--namespace", "default", "get", "configmap", "demo")
 }
 
+func TestExportEnvironment(t *testing.T) {
+	tmpDir := t.TempDir()
+	os.Chdir(tmpDir)
+	runCmd(t, "tk", "init")
+	runCmd(t, "tk", "env", "set", "environments/default", "--server=https://kubernetes:6443")
+	content := `
+	{
+		config: {
+	         apiVersion: "v1",
+	         kind: "ConfigMap",
+	         metadata : {
+	              name: "demo",
+	         },
+	         data: {},
+		},
+	}
+`
+	require.NoError(t, os.WriteFile("environments/default/main.jsonnet", []byte(content), 0600))
+	runCmd(t, "tk", "export", "export", "environments/default")
+	require.FileExists(t, "export/v1.ConfigMap-demo.yaml")
+}
+
 func runCmd(t *testing.T, cmd string, args ...string) {
 	c := exec.Command(cmd, args...)
 	c.Stdout = os.Stdout
