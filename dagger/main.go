ce3be8f6540c03333f2c7cefe43a8ed921ddb784
Horst Gutmann <horst.gutmann@grafana.com>
2024-06-26 15:52:38 +0200

    Use k3s as internal k8s cluster

--- dagger/main.go
+++ dagger/main.go
@@ -16,6 +16,7 @@ package main
 
 import (
 	"context"
+	"time"
 )
 
 type Tanka struct{}
@@ -26,13 +27,23 @@ func (m *Tanka) Build(ctx context.Context, rootDir *Directory) *Container {
 }
 
 func (m *Tanka) AcceptanceTests(ctx context.Context, rootDir *Directory) (string, error) {
+	k3s := dag.K3S("k3sdemo")
+	k3sSrv, err := k3s.Server().Start(ctx)
+	if err != nil {
+		return "", err
+	}
+	defer k3sSrv.Stop(ctx)
 	output, err := dag.Container().
 		From("golang:1.22-alpine").
+		WithExec([]string{"apk", "add", "--no-cache", "kubectl"}).
 		WithMountedFile("/usr/bin/tk", m.Build(ctx, rootDir).File("/usr/local/bin/tk")).
 		WithMountedDirectory("/tests", rootDir.Directory("acceptance-tests")).
+		WithEnvVariable("CACHE", time.Now().String()).
+		WithServiceBinding("kubernetes", k3sSrv).
+		WithFile("/root/.kube/config", k3s.Config(false)).
 		WithWorkdir("/tests").
+		WithExec([]string{"sed", "-i", `s/https:.*:6443/https:\/\/kubernetes:6443/g`, "/root/.kube/config"}).
 		WithExec([]string{"go", "test", "./...", "-v"}).
 		Stdout(ctx)
 	return output, err
-
 }
