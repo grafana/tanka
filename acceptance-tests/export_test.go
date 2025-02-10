348353cbb2b185ff6e565f1b453152ad9867bef6
Horst Gutmann <horst.gutmann@grafana.com>
2025-02-10 14:28:08 +0100

    tests: add acceptance test for tk export -l

--- acceptance-tests/export_test.go
+++ acceptance-tests/export_test.go
@@ -5,14 +5,16 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestExportEnvironment(t *testing.T) {
-	tmpDir := t.TempDir()
-	runCmd(t, tmpDir, "tk", "init")
-	runCmd(t, tmpDir, "tk", "env", "set", "environments/default", "--server=https://kubernetes:6443")
-	content := `
+	t.Run("basic", func(t *testing.T) {
+		tmpDir := t.TempDir()
+		runCmd(t, tmpDir, "tk", "init")
+		runCmd(t, tmpDir, "tk", "env", "set", "environments/default", "--server=https://kubernetes:6443")
+		content := `
 	{
 		config: {
 	         apiVersion: "v1",
@@ -24,7 +26,49 @@ func TestExportEnvironment(t *testing.T) {
 		},
 	}
 `
-	require.NoError(t, os.WriteFile(filepath.Join(tmpDir, "environments/default/main.jsonnet"), []byte(content), 0600))
-	runCmd(t, tmpDir, "tk", "export", "export", "environments/default")
-	require.FileExists(t, filepath.Join(tmpDir, "export/v1.ConfigMap-demo.yaml"))
+		require.NoError(t, os.WriteFile(filepath.Join(tmpDir, "environments/default/main.jsonnet"), []byte(content), 0600))
+		runCmd(t, tmpDir, "tk", "export", "export", "environments/default")
+		require.FileExists(t, filepath.Join(tmpDir, "export/v1.ConfigMap-demo.yaml"))
+	})
+
+	t.Run("only-labeled", func(t *testing.T) {
+		tmpDir := t.TempDir()
+		runCmd(t, tmpDir, "tk", "init", "--inline")
+
+		// We have two environments stored here but we only want the one with
+		// the label "wanted" set to true:
+		content := `
+	{
+		environment(name):: {
+			apiVersion: 'tanka.dev/v1alpha1',
+			kind: 'Environment',
+			metadata: {
+			  name: 'environment/%s' % (name),
+			  labels: {
+				  'wanted': (if name == 'wanted' then 'true' else 'false'),
+			  },
+			},
+			spec: {
+			  namespace: 'test-%s' % (name),
+			  inline: "true",
+			},
+			data: {
+				config: {
+					 apiVersion: "v1",
+					 kind: "ConfigMap",
+					 metadata : {
+						  name: "demo",
+					 },
+					 data: {},
+				 },
+			},
+		},
+		envs: [$.environment('wanted'), $.environment('unwanted')],
+	}
+`
+		require.NoError(t, os.WriteFile(filepath.Join(tmpDir, "environments/default/main.jsonnet"), []byte(content), 0600))
+		runCmd(t, tmpDir, "tk", "export", "--recursive", "-l", "wanted=true", "--format", "{{ .metadata.namespace }}/{{.apiVersion}}.{{.kind}}-{{or .metadata.name .metadata.generateName}}", "export", "environments/default")
+		assert.FileExists(t, filepath.Join(tmpDir, "export/test-wanted/v1.ConfigMap-demo.yaml"))
+		assert.NoFileExists(t, filepath.Join(tmpDir, "export/test-unwanted/v1.ConfigMap-demo-unwanted.yaml"))
+	})
 }
